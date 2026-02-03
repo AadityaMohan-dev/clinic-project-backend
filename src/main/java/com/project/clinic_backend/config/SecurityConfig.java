aab927c978819279e7bf62c21c3f3a1c97d82337
AadityaMohan <aadimohan51@gmail.com>
2026-02-03 15:27:40 +0530

    update appointment response dto

--- src/main/java/com/project/clinic_backend/config/SecurityConfig.java
+++ src/main/java/com/project/clinic_backend/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -42,14 +43,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/v1/**").permitAll()   // public auth APIs
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .anyRequest().authenticated()   // everything else requires JWT
+                        .anyRequest().authenticated()
                 )
 
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
--- src/main/java/com/project/clinic_backend/models/dtos/AppointmentResponseDto.java
+++ src/main/java/com/project/clinic_backend/models/dtos/AppointmentResponseDto.java
@@ -1,8 +1,38 @@
 package com.project.clinic_backend.models.dtos;
 
+import com.project.clinic_backend.models.enums.AppointmentStatus;
+import com.project.clinic_backend.models.enums.AppointmentType;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Builder
 public record AppointmentResponseDto(
-        String userId,
-        String message,
-        String username
-) {
-}
+
+        UUID appointmentId,
+
+        String fullName,
+
+        String email,
+
+        String phoneNumber,
+
+        LocalDate appointmentDate,
+
+        LocalTime appointmentTime,
+
+        String reasonForVisit,
+
+        String additionalNotes,
+
+        AppointmentType appointmentType,
+
+        AppointmentStatus status,
+
+        LocalDateTime createdAt,
+
+        LocalDateTime updatedAt
+) {}
